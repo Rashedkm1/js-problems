2abfc8e31cd2389007a25ed9ce258f8d8ededa8e
Rashedkm1 <dev.rashedkm@gmail.com>
2023-03-25 00:21:12 +0600

    Create a function that will return a Boolean specifying if a number is prime

--- challenge16.js
+++ challenge16.js
@@ -1,4 +1,5 @@
 //Create a function that will return a Boolean specifying if a number is prime
+// মৌলিক সংখ্যা বের করার অনেক নিয়ম রয়েছে তবে সবচেয়ে সহজ পদ্ধতি হচ্ছে পরীক্ষামূলক ভাগ। এখানে সংখ্যাকে n ধরা হয় এবং ২ থেকে শুরু করে n এর বর্গমূল পর্যন্ত কোনো দুইটি সংখ্যার গুনফল কিনা তা দেখতে হয়।
 
 function isPrime(num) {
     if(num <= 1){
@@ -19,5 +20,26 @@ function isPrime(num) {
 
 console.log(isPrime(7));
 
+/** 
+//Another Way
+function isPrime(n) {
+    if (n < 2)
+        return false;
 
+    if (n == 2)
+        return true;
 
+    var maxDiv = Math.sqrt(n);
+
+    for (var i = 2; i <= maxDiv; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(2, " is prime? ", isPrime(2));
+
+*/
